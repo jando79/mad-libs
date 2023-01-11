8d63ebc42f4f19850f4a82d21640735240c5902a
David Jandron <jandron1211@gmail.com>
2023-01-11 10:47:53 -0800

    added notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
+    //captures the inputs
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
     const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
     const nounInput = document.getElementById("nounInput").value;
-    document.querySelector("span#person1a").innerText = "person 1";
-    document.querySelector("span#person1b").innerText = "person 1";
-    document.querySelector("span#person1c").innerText = "person 1";
-    document.querySelector("span#person2a").innerText = "person 2";
-    document.querySelector("span#person2b").innerText = "person 2";
-    document.querySelector("span#animal").innerText = "an animal";
-    document.querySelector("span#verb").innerText = "verb";
-    document.querySelector("span#noun").innerText = "noun";
-    document.querySelector("span#exclamation").innerText = "exclamation";
-
-    // then we show the story by removing the class attribute
+    //says what to do with input
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+    //removes the hidden value in css once submit is clicked
     document.querySelector("div#story").removeAttribute("class");
+    //prevents page from reloading when submit is clicked
     event.preventDefault();
   };
 };
